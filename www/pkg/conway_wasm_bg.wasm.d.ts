f4616a7b1658ffa593d5dec3b430ef15fb0a3957
rab <rab@andybeverly.com>
2025-06-05 02:23:13 -0400

    add locally built wasm

---
+++ www/pkg/conway_wasm_bg.wasm.d.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_game_free: (a: number, b: number) => void;
+export const game_new: () => number;
+export const game_randomize: (a: number) => void;
+export const game_clear: (a: number) => void;
+export const game_start: (a: number) => void;
+export const game_stop: (a: number) => void;
+export const game_set_cell: (a: number, b: number, c: number, d: number) => void;
+export const game_render: (a: number, b: number, c: number) => void;
+export const game_update: (a: number) => void;
+export const game_is_running: (a: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_start: () => void;
